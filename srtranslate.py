353314906d75e41853da6ef34bd27cba2c44e39d
mathaew <hardway3@protonmail.com>
2019-07-16 16:40:36 -0300

    Code update

--- srtranslate.py
+++ srtranslate.py
@@ -1,20 +1,25 @@
 import argparse
-
-from util.translate import Translate, Colors
+import googletrans
+from util.translate import translate, translate_file, Colors
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--dir', help='Directory where the files are located. (.srt)', required=False)
-parser.add_argument('-s', '--single', help='File for translation', required=False)
-parser.add_argument('-l', '--language', help='Please enter your language. example: english = en, Portuguese = en. etc...')
+parser.add_argument('-d', '--dir', help='Directory where the files are located. (.srt)', type=str, required=False)
+parser.add_argument('-s', '--single', help='File for translation', type=str, required=False)
+parser.add_argument('-l', '--language', help='Please enter your language. example: english = en, Portuguese = pt, German = de etc...', type=str,required=False)
+parser.add_argument('-c', '--codes', help="View all language codes", action='store_true')
 args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    if args.dir == None and args.single == None:
-        print(Colors.FAIL + 'No file or directory was passed' + Colors.ENDC)
-    elif args.dir:
-        Translate(args.dir, args.language, False)
-    elif args.single:
-        Translate(args.single, args.language)
+
+    if args.codes:
+        print(googletrans.LANGCODES)
     else:
-        print(Colors.FAIL + 'No file or directory was passed' + Colors.ENDC)
+        if args.dir == None and args.single == None:
+            print(Colors.FAIL + 'No file or directory was passed' + Colors.ENDC)
+        elif args.dir:
+            translate(args.dir, args.language)
+        elif args.single:
+            translate_file(args.single, args.language)
+        else:
+            print(Colors.FAIL + 'No file or directory was passed' + Colors.ENDC)
--- util/translate.py
+++ util/translate.py
@@ -1,4 +1,5 @@
 from googletrans import Translator
+from googletrans import LANGUAGES
 import os
 
 class Colors:
@@ -11,73 +12,88 @@ class Colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+translator = Translator(service_urls=[
+        'translate.google.com',
+        'translate.google.com.br'
+    ])
 
-def Translate(path, language, single=True):
-    translator = Translator(service_urls=[
-          'translate.google.com',
-          'translate.google.com.br'
-        ])
+
+def codes(code_lang:str):
+    for code, lang in LANGUAGES.items():
+        if code == code_lang:
+            return True
+    return False
+
+
+def translate(path:str, language:str):
+
+    if (not codes(language)):
+        print(Colors.FAIL + "")
+        return
 
     abspath = os.path.abspath(path)
-    if (single):
+
+    if not os.path.exists(abspath + '/' + 'translated_legends'):
+        os.mkdir(abspath + '/' + 'translated_legends')
+    fileslist = os.listdir(abspath)
+    for files in fileslist:
         phrase = []
-        print(Colors.WARNING + 'The {} file is being translated'.format(abspath.split('/')[-1]) + Colors.ENDC)
-        with open(abspath) as file:
-            for lines in file:
-                if lines[0].rstrip().isalpha():
-                    phrase.append(lines.rstrip())
-            file.close()
+        filename = ''
 
-        traducao = translator.translate(phrase, dest=language)
+        if files == 'translated_legends':
+            continue
 
-        count = 0
-        with open(language + '-' + path, 'w') as save:
-            with open(abspath) as file:
+        if not files.split('.')[-1] == '.str':
+            filename = files
+            print(Colors.WARNING + 'The {} file is being translated'.format(files) + Colors.ENDC)
+            with open(abspath + '/' + files) as file:
                 for lines in file:
                     if lines[0].rstrip().isalpha():
-                        save.write(traducao[count].text + '\n')
-                        count += 1
-                    else:
-                        save.write(lines)
+                        phrase.append(lines.rstrip())
                 file.close()
-            save.close()
-        print(Colors.OKGREEN + 'The file {} was translated'.format(abspath.split('/')[-1]) + Colors.ENDC)
 
-    else:
-        if not os.path.exists(abspath + '/' + 'translated_legends'):
-            os.mkdir(abspath + '/' + 'translated_legends')
-        fileslist = os.listdir(abspath)
-        for files in fileslist:
-            phrase = []
-            filename = ''
+            traducao = translator.translate(phrase, dest=language)
 
-            if files == 'translated_legends':
-                continue
-
-            if not files.split('.')[-1] == '.str':
-                filename = files
-                print(Colors.WARNING + 'The {} file is being translated'.format(files) + Colors.ENDC)
+            count = 0
+            with open(abspath + '/' + 'translated_legends' + '/' + files, 'w') as save:
                 with open(abspath + '/' + files) as file:
                     for lines in file:
                         if lines[0].rstrip().isalpha():
-                            phrase.append(lines.rstrip())
+                            save.write(traducao[count].text + '\n')
+                            count += 1
+                        else:
+                            save.write(lines)
                     file.close()
+                save.close()
+            print(Colors.OKGREEN + 'The file {} was translated'.format(filename)+Colors.ENDC)
+        else:
+            continue
+    print(Colors.OKGREEN + 'Files have been saved in {}'.format(abspath + '/' + 'translated_legends'))
 
-                traducao = translator.translate(phrase, dest=language)
 
-                count = 0
-                with open(abspath + '/' + 'translated_legends' + '/' + files, 'w') as save:
-                    with open(abspath + '/' + files) as file:
-                        for lines in file:
-                            if lines[0].rstrip().isalpha():
-                                save.write(traducao[count].text + '\n')
-                                count += 1
-                            else:
-                                save.write(lines)
-                        file.close()
-                    save.close()
-                print(Colors.OKGREEN + 'The file {} was translated'.format(filename)+Colors.ENDC)
-            else:
-                continue
-        print(Colors.OKGREEN + 'Files have been saved in {}'.format(abspath + '/' + 'translated_legends'))
+def translate_file(path:str, language:str):
 
+    abspath = os.path.abspath(path)
+
+    phrase = []
+    print(Colors.WARNING + 'The {} file is being translated'.format(abspath.split('/')[-1]) + Colors.ENDC)
+    with open(abspath) as file:
+        for lines in file:
+            if lines[0].rstrip().isalpha():
+                phrase.append(lines.rstrip())
+        file.close()
+
+    traducao = translator.translate(phrase, dest=language)
+
+    count = 0
+    with open(language + '-' + path, 'w') as save:
+        with open(abspath) as file:
+            for lines in file:
+                if lines[0].rstrip().isalpha():
+                    save.write(traducao[count].text + '\n')
+                    count += 1
+                else:
+                    save.write(lines)
+            file.close()
+        save.close()
+    print(Colors.OKGREEN + 'The file {} was translated'.format(abspath.split('/')[-1]) + Colors.ENDC)
